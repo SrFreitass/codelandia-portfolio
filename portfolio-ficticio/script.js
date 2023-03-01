88f07d1839b0e63b11ad0b1951e9f1be628f8931
Guilherme Freitas do Nascimento <121752740+SrFreitass@users.noreply.github.com>
2023-03-01 16:23:07 -0400

    Delete portfolio-ficticio directory

--- portfolio-ficticio/script.js
+++
@@ -1,9 +0,0 @@
-const menuMobile = document.getElementById('menu-mobile')
-const menuMobileclose = document.getElementById('menu-mobile-close')
-menuMobile.addEventListener('click', menuActive) 
-menuMobileclose.addEventListener('click', menuActive)
-
-function menuActive() {
-    const header = document.getElementById('header')
-    header.classList.toggle('active')
-}
